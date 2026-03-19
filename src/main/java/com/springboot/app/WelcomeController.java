84e43e15dc08092a2090a9225bd3a7168f47bb93
Ankita <ankitagupta062003@gmail.com>
2026-03-19 20:12:39 +0530

    Add files via upload

---
+++ src/main/java/com/springboot/app/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.springboot.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController ;
+
+@RestController
+public class WelcomeController {
+	
+	@GetMapping("/welcome")
+	public String Welcome() {
+		return"Welcome to spring boot";
+		
+
+}
+}
